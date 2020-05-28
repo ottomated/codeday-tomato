62195e6bade2137e583bb640acfb9d93d48a1a57
Ottomated <otto@ottomated.net>
2020-05-28 01:32:19 -0700

    Basic platformer script

---
+++ scripts/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+export (int) var GRAVITY = 1800;
+export (int) var MAX_SPEED = 200;
+export (int) var JUMP_SPEED = -400;
+export (int) var ACCEL = 100;
+export (int) var DEACCEL = 200;
+const MAX_SLOPE_ANGLE = 40;
+
+var vel = Vector2();
+
+func _physics_process(delta):
+	
+	var hvel = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		vel.y = JUMP_SPEED;
+	
+	vel.y += delta * GRAVITY;
+	
+	var target = hvel * MAX_SPEED;
+	
+	var accel = ACCEL if sign(vel.x) == sign(target) else DEACCEL;
+	
+	hvel = hvel + (target - hvel) * (accel * delta);
+	vel.x = hvel;
+	
+	vel = move_and_slide(vel, Vector2.UP, false, 4, deg2rad(MAX_SLOPE_ANGLE));
