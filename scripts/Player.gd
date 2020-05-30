15590d3b9273619125ee64c121b2c6a1a2236a18
Ottomated <otto@ottomated.net>
2020-05-29 17:59:42 -0700

    Attack keybindings

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,8 +11,7 @@ const MAX_SLOPE_ANGLE = 40;
 
 var vel = Vector2();
 
-func _physics_process(delta):
-	
+func _do_movement(delta):
 	var hvel = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
 	if is_on_floor() and Input.is_action_pressed("move_jump"):
@@ -24,7 +23,6 @@ func _physics_process(delta):
 	elif vel.y < 0 and not Input.is_action_pressed("move_jump"):
 		gravity_mult = LOW_JUMP_MULT;
 		
-	print(gravity_mult);
 	vel.y += delta * GRAVITY * gravity_mult;
 	
 	var target = hvel * MAX_SPEED;
@@ -35,3 +33,13 @@ func _physics_process(delta):
 	vel.x = hvel;
 	
 	vel = move_and_slide(vel, Vector2.UP, false, 4, deg2rad(MAX_SLOPE_ANGLE));
+
+func _do_attacks(delta):
+	if Input.is_action_just_pressed("attack_primary"):
+		print("ATTACK");
+	if Input.is_action_just_pressed("attack_secondary"):
+		print("SECONDARY ATTACK");
+
+func _physics_process(delta):
+	_do_movement(delta);
+	_do_attacks(delta);
